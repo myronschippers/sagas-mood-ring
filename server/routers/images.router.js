f294a44314e32bedbef57106a1565a26c85314c1
Myron <dubtwee@gmail.com>
2019-05-25 11:46:38 -0500

    making some basic router that can be fleshed out later for the API interactions

---
+++ server/routers/images.router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const pool = require('../modules/pool');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+    
+});
+
+router.put('/', (req, res) => {
+    
+});
+
+router.delete('/', (req, res) => {
+    
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
+const tags = require('./routers/tags.router');
+const images = require('./routers/images.router');
+const basePath = '/api';
 
 /** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
-
+app.use(`${basePath}/tags`, tags);
+app.use(`${basePath}/images`, images);
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
