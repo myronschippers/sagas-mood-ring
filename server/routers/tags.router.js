9241a165b1b24ccfdc59a030a5440125f1b052fe
Myron <dubtwee@gmail.com>
2019-05-25 17:52:16 -0500

    creating api calls for getting the list of tags from the DB and posting a new tag to the DB

--- server/routers/tags.router.js
+++ server/routers/tags.router.js
@@ -4,18 +4,39 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    const dbQuery = `SELECT * FROM "tags";`;
 
+    pool.query(dbQuery)
+        .then((response) => {
+            console.log('get images: ', response);
+            res.send(response.rows);
+        })
+        .catch((err) => {
+            console.log('Error getting images: ', err);
+        });
 });
 
 router.post('/', (req, res) => {
-    
+    const saveData = req.body;
+    const dbQuery = `INSERT INTO "tags" ("name")
+                    VALUES $1;`;
+
+    pool.query(dbQuery, [saveData.imageId, saveData.tagId])
+        .then((response) => {
+            console.log('post tag to image: ', response);
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('Error posting to: ', err);
+            res.sendStatus(500);
+        });
 });
 
 router.put('/', (req, res) => {
     
 });
 
-router.delete('/', (req, res) => {
+router.delete('/:id', (req, res) => {
     
 });
 
