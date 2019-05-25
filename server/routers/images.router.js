43825289753d09f8132bc9af94a0344acc151276
Myron <dubtwee@gmail.com>
2019-05-25 17:43:45 -0500

    creating a get for all images, one for all images with tags, and a final put for adding a tag to an image

--- server/routers/images.router.js
+++ server/routers/images.router.js
@@ -3,12 +3,51 @@ const pool = require('../modules/pool');
 
 const router = express.Router();
 
+// base route returns all of the images without associated tags
 router.get('/', (req, res) => {
+    const dbQuery = `SELECT * FROM "images";`;
 
+    pool.query(dbQuery)
+        .then((response) => {
+            console.log('get images: ', response);
+            res.send(response.rows);
+        })
+        .catch((err) => {
+            console.log('Error getting images: ', err);
+        })
 });
 
-router.post('/', (req, res) => {
-    
+router.get('/withtags', (req, res) => {
+    const dbQuery = `SELECT * FROM "images"
+                    JOIN "junc_images_tags" ON "images"."id"="junc_images_tags"."image_id"
+                    JOIN "tags" ON "junc_images_tags"."tag_id"="tags"."id";`;
+
+    pool.query(dbQuery)
+        .then((response) => {
+            console.log('get images: ', response);
+            res.send(response.rows);
+        })
+        .catch((err) => {
+            console.log('Error getting images: ', err);
+            res.sendStatus(500);
+        })
+});
+
+// Make a relationship between an image and a tag using the junction table.
+router.post('/addtag', (req, res) => {
+    const saveData = req.body;
+    const dbQuery = `INSERT INTO "junc_images_tags" ("image_id", "tag_id")
+                    VALUES $1, $2;`;
+
+    pool.query(dbQuery, [saveData.imageId, saveData.tagId])
+        .then((response) => {
+            console.log('post tag to image: ', response);
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('Error posting to: ', err);
+            res.sendStatus(500);
+        });
 });
 
 router.put('/', (req, res) => {
